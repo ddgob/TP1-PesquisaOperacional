e3c88785f33d9caadfaee6c50709fa287e9aed06
ddogs <danielolibar@gmail.com>
2024-03-27 15:14:36 -0300

    Added feature to read and validate command line arguments for maximum clique problem

--- Problema de Clique Maxima/tp1_2020006450.jl
+++ Problema de Clique Maxima/tp1_2020006450.jl
@@ -35,4 +35,15 @@ function processInstance(instancePath)
 
 end
 
-numberOfVertices, graph = processInstance("./Problema de Clique Maxima/exemplosCliqueMaxima/instancias/INST1_PB2.txt")
+# Validate command line arguments
+if length(ARGS) != 1 && length(ARGS) != 2
+    println("Usage: julia tp1_2020006450.jl <path_to_instance_of_problem> <optional_true_to_output_vertice_solution_set>")
+    exit(1)
+end
+
+# Extract the path to the instance file 
+# from the command line arguments
+instancePath = ARGS[1]
+lenArgs = length(ARGS)
+
+numberOfVertices, graph = processInstance(instancePath)
